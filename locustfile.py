08fde05f606071871cec895589ca7173b7136113
MUHAMMED FIROZ A A <108992155+Muhammed-Firoz@users.noreply.github.com>
2022-09-28 19:57:05 +0530

    Update locustfile.py
    added a test at /prediction end

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,22 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, SequentialTaskSet, constant
 
-class HelloWorldUser(HttpUser):
+class flaskAppTester(SequentialTaskSet):
+    #Sequentially execute the task
     @task
-    def hello_world(self):
-        self.client.get("https://firozflaskmlapp.azurewebsites.net")
-       
+    def get_users(self):
+        res = self.client.get("/")
+        print("Landing page status is ", res.status_code)
+    
+        
+    @task
+    def post_prediction(self):
+        res=self.client.post("/predict")
+        print("Predict end status is ",res.status_code)
+        
+        
+class MyAppTester(HttpUser):
+    wait_time = constant(1)
+    host = "https://firozflaskmlapp.azurewebsites.net"
+    
+    
+    tasks = [flaskAppTester]
